e6223555e07287524734357855a3757d39c8686a
Moonshine-sh <66140132+Moonshine-sh@users.noreply.github.com>
2021-11-18 15:59:26 +0300

    test fix

--- src/test/java/by/ginel/autopark/CarTest.java
+++ src/test/java/by/ginel/autopark/CarTest.java
@@ -5,9 +5,16 @@ import by.ginel.autopark.model.Parkinglot;
 import by.ginel.autopark.model.Ticket;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Answers;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+import javax.xml.crypto.Data;
+import java.security.PublicKey;
+import java.util.Date;
 import java.util.concurrent.Exchanger;
+import java.util.concurrent.Semaphore;
 
 import static java.lang.Thread.sleep;
 
@@ -17,9 +24,16 @@ public class CarTest {
     public void runTest() throws Exception{
         Parkinglot parkinglot = Mockito.mock(Parkinglot.class);
         Car car = new Car(1, parkinglot, new Exchanger<Ticket>());
-        Mockito.when(parkinglot.checkIn()).thenReturn(null);
+        Ticket ticket = new Ticket();
+        if(Math.random()*100 > 50){
+            ticket.setSpotNum(1);
+            ticket.setDateOfExp(new Date(new Date().getTime()));
+        }
+        else
+            ticket = null;
+        Mockito.when(parkinglot.checkIn()).thenReturn(ticket);
         car.start();
-        sleep(200);
+        sleep(500);
 
         Assert.assertFalse(car.isAlive());
     }
